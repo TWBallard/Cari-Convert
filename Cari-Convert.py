18032452e6b737d50ece2ce0762694c0e0031cf0
Tom Ballard <tom@twballard.com>
2021-06-23 18:43:34 +0100

    Stub convert function

--- Cari-Convert.py
+++ Cari-Convert.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 
 parser = argparse.ArgumentParser(description="Simple Python Console App To Convert Some Values")
 parser.add_argument('Quantity', default="1", help="The Number of Source Units")
 parser.add_argument('SourceUnit', default="Meter", help="The Source Unit")
 parser.add_argument('TargetUnit', default="Kilometer", help="The Target Units")
+parser.add_argument('--db', default="units.csv", help="The file containing the Database")
 
 def main():
     args = parser.parse_args()
+    db = csv.DictReader(open(args.db))
 
-    print(f'{args.Quantity}  {args.SourceUnit}  is x many {args.TargetUnit}')
+    #Perform Conversion
+    x = convert(args.Quantity, args.SourceUnit, args.TargetUnit, db)
+
+    #Output Conversion
+    print(f'{args.Quantity}  {args.SourceUnit}  is {x} many {args.TargetUnit}')
+
+def convert(Quantity, SourceUnit, TargetUnit, db):
+
+    x = "42"
+
+    return(x)
 
 
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# Cari-Convert
+## Introduction
+Simple Python Application to confirm units. Written as coding challenge during application
