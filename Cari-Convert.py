243d7d9503ebff2626c1c73d5756f5404c75e1ad
TWBallard <86123023+TWBallard@users.noreply.github.com>
2021-06-23 20:00:08 +0100

    Feature/read (#1)
    * Stub convert function
    
    * Read and return factor

--- Cari-Convert.py
+++ Cari-Convert.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
 import argparse
+import pandas as pandaReader
 
 parser = argparse.ArgumentParser(description="Simple Python Console App To Convert Some Values")
 parser.add_argument('Quantity', default="1", help="The Number of Source Units")
 parser.add_argument('SourceUnit', default="Meter", help="The Source Unit")
 parser.add_argument('TargetUnit', default="Kilometer", help="The Target Units")
+parser.add_argument('--db', default="units.csv", help="The file containing the Database")
 
 def main():
     args = parser.parse_args()
 
-    print(f'{args.Quantity}  {args.SourceUnit}  is x many {args.TargetUnit}')
+    #Read DB to return Factor
+    factor = readdb(args.SourceUnit, args.TargetUnit, args.db)
+    print(f'factor is {factor}')
+
+    #Perform Conversion
+    x = convert(args.Quantity, args.SourceUnit, args.TargetUnit)
+
+    #Output Conversion
+    #print(f'{args.Quantity}  {args.SourceUnit}  is {x} many {args.TargetUnit}')
+
+def readdb(SourceUnit, TargetUnit, dbPath):
+    db = pandaReader.read_csv(dbPath)
+    #print(f'output: {db.iloc[0].SourceUnit}')
+    for index, row in db.iterrows():
+        if row.SourceUnit == SourceUnit and row.TargetUnit == TargetUnit:
+            return(row.Factor)
+
+
+def convert(Quantity, SourceUnit, TargetUnit):
+
+    x = "42"
+
+    return(x)
+
+
 
 
 
